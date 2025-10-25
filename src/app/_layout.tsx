f5ef8027b2862aadf92a657e3991bb2b7f551a4d
Arnold <arnold@whakatau.com>
2025-10-25 22:18:08 +0200

    default auth flow

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Slot } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env",
+  );
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Slot />
+      </SafeAreaView>
+    </ClerkProvider>
+  )
 }
